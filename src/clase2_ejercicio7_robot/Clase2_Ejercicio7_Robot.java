e4833ea86bd4fc442cbe7878169a730b77c0b6e2
RobertoRivera23 <140562758+RobertoRivera23@users.noreply.github.com>
2023-08-08 18:13:15 -0300

    Update Clase2_Ejercicio7_Robot.java

--- src/clase2_ejercicio7_robot/Clase2_Ejercicio7_Robot.java
+++ src/clase2_ejercicio7_robot/Clase2_Ejercicio7_Robot.java
@@ -1,28 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package clase2_ejercicio7_robot;
 
-/**
- *
- * @author rober
- */
+
 public class Clase2_Ejercicio7_Robot {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
-    Robot R = new Robot ("Tito");
-    double pasos =1;
-        System.out.println("Hola mundo!");
-    //while (!R.bateriaVacia()){
-            
-        //System.out.println(" "+R.avanzar(200));
-    
+  
     }
 }    
     
